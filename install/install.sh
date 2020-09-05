c91902c354467b029dab2d1c8abb4bb8fcfd7c7f
acavella <acavella@gmail.com>
2020-09-05 16:13:39 -0400

    add CRL_date to db

--- install/install.sh
+++ install/install.sh
@@ -97,7 +97,8 @@ CREATE TABLE crlList (
         Row_ID integer PRIMARY KEY AUTOINCREMENT,
         CRL_Uri text,
         CRL_Name text,
-        CRL_Hash text
+        CRL_Hash text,
+        CRL_Date text
 );
 END_SQL
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Database initialization completed." 2>&1 | tee -a $logFile
